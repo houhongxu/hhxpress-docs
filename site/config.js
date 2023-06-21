c932fc114280a8f0ecde0f253134ce5a8610fbb2
HHX <903040380@qq.com>
2023-06-21 10:30:12 +0800

    feat: 初始化项目

---
+++ site/config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'hhxpress'
+
+export default defineConfig({
+  title: 'HHX的网站',
+  themeConfig: {
+    nav: {
+      title: 'HHXPRESS',
+      items: [{ text: '指南', link: '/guide/intro' }],
+    },
+  },
+})
