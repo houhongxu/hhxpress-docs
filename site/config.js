cb1e5448da60c20fd606b0c104ecfdafcafeab2f
HHX <903040380@qq.com>
2023-06-21 15:43:50 +0800

    chore: 更新文章顺序

--- site/config.js
+++ site/config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   themeConfig: {
     nav: {
       title: 'HHXPRESS',
-      items: [{ text: '指南', link: '/guide/介绍/1简介' }],
+      items: [{ text: '指南', link: '/guide/0介绍/1简介' }],
     },
   },
 })
